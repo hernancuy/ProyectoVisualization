eda7109ef352e4d6655b8e21808ef86a1fbdbacf
Hernan David Cuy <60525731+hernancuy@users.noreply.github.com>
2022-12-04 12:49:18 -0500

    Update app.js
    Cambios nueva visualización

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/ProyectoFinal_v2022_2/Cancerunacondicinsindistincin?:language=es-ES&publish=yes&:display_count=n&:origin=viz_share_link";
+const url = "https://public.tableau.com/views/ISIS4822_VisualAnalytics_Proyecto-cncerdemama_Dashboard/Cancerunacondicinsindistincin?:language=es-ES&publish=yes&:display_count=n&:origin=viz_share_link";
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
